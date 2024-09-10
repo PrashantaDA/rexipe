1841e9dcc6247051ddb7fcfa47c4ad88f9d95b4e
Prashanta D Acharya <apras.dixit@gmail.com>
2024-09-11 00:11:21 +0545

    Added progressive loading

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -5,6 +5,7 @@ import RecipeGrid from "../components/RecipeGrid";
 const Searched = () => {
 	const { search } = useParams();
 	const [searchedRecipes, setSearchedRecipes] = useState([]);
+	const [visibleCount, setVisibleCount] = useState(8); // Number of items to display initially
 	const [loading, setLoading] = useState(true); // Add loading state
 
 	const getSearched = async (query) => {
@@ -19,15 +20,33 @@ const Searched = () => {
 		getSearched(search);
 	}, [search]);
 
+	const handleLoadMore = () => {
+		setVisibleCount((prevCount) => prevCount + 8); // Increase the number of visible items
+	};
+
+	const shouldShowLoadMore = searchedRecipes.length > visibleCount;
+
 	return (
 		<>
 			{loading ? (
 				<p className="flex justify-center h-screen text-center mt-8 text-2xl font-bold text-dark">Loading...</p>
 			) : searchedRecipes.length > 0 ? (
-				<RecipeGrid
-					title={`Search Results for "${search}"`}
-					items={searchedRecipes}
-				/>
+				<>
+					<RecipeGrid
+						title={`Search Results for "${search}"`}
+						items={searchedRecipes.slice(0, visibleCount)} // Display only up to visibleCount items
+					/>
+					{shouldShowLoadMore && (
+						<div className="flex justify-center mt-8">
+							<button
+								className="px-4 py-2 bg-dark text-primarywhite font-bold rounded my-4"
+								onClick={handleLoadMore}
+							>
+								View More
+							</button>
+						</div>
+					)}
+				</>
 			) : (
 				<p className="text-center mt-8 text-lg text-dark">No recipes found for &quot;{search}&quot;.</p>
 			)}
